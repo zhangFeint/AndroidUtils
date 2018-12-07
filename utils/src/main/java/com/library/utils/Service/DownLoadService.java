7239a2b24ec9ebf290de67f2e6fe4681d56cf225
13674928326 <695584109@qq.com>
2018-12-07 15:30:00 +0800

    添加下载文件服务

--- utils/src/main/java/com/library/utils/Service/DownLoadService.java
+++ utils/src/main/java/com/library/utils/Service/DownLoadService.java
@@ -1,14 +1,17 @@
 package com.library.utils.Service;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+
+import com.library.utils.R;
 import com.library.utils.httpservice.HttpRequestUtils;
 import com.library.utils.httpservice.OkHttp3NetWork;
 import com.library.utils.utils.SystemUtils;
@@ -18,51 +21,59 @@ import java.io.File;
 /**
  * 软件下载安装
  * Intent intent = new Intent(this, DownLoadService.class);
- * intent.putExtra(DownLoadService.FILE_URL_SIGN, "http://ucan.25pp.com/Wandoujia_web_seo_baidu_homepage.apk");
+ * intent.putExtra(DownLoadService.KEY_URL_SIGN, "http://ucan.25pp.com/Wandoujia_web_seo_baidu_homepage.apk");
  * intent.putExtra(DownLoadService.FILE_DOWNLOAD_TIBLE, "tible");
  * startService(intent);
  */
 public class DownLoadService extends Service {
+    private final String TAG = "DownLoadService";
     private Context mContext;
+    public int notifyId ;//通知栏id
 
-    //通知栏
-    private NotificationCompat.Builder builder;
-    private NotificationManager notificationManager;
-    private int preProgress = 0;
+    private String tible = "";
 
-    private String tible = "更新包下载中...";
-    private int icon = -1;
-    public int notify_id = 1000;//通知栏id
     //数据传输的标记
-    public static final String FILE_URL_SIGN = "url";
-    public static final String FILE_DOWNLOAD_TIBLE = "tible";
-    public static final String FILE_NOTIFY_ID = "notifyID";//通知栏id
+    public static final String KEY_URL_SIGN = "url";
+    public static final String KEY_TIBLE = "tible";
+    public static final String KEY_ICON = "icon";
+    public static final String KEY_NOTIFY_ID = "notifyID";//通知栏id
+
+    public static void show(Context context, @DrawableRes int icon, String url, String tible, int notifyId) {
+        Intent intent = new Intent(context, DownLoadService.class);
+        intent.putExtra(KEY_URL_SIGN, url);
+        intent.putExtra(KEY_TIBLE, tible == null ? "更新包下载中..." : tible);
+        intent.putExtra(KEY_ICON, icon );
+        intent.putExtra(KEY_NOTIFY_ID, notifyId == -1 ? 1000 : notifyId);
+        context.startService(intent);
+    }
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         mContext = this;
-        String url = intent.getStringExtra(FILE_URL_SIGN);
-        notify_id = intent.getIntExtra(FILE_NOTIFY_ID, 1000);
-        tible = intent.getStringExtra(FILE_DOWNLOAD_TIBLE) == null ? tible : intent.getStringExtra(FILE_DOWNLOAD_TIBLE);
-        tible = intent.getStringExtra(FILE_DOWNLOAD_TIBLE) == null ? tible : intent.getStringExtra(FILE_DOWNLOAD_TIBLE);
-        initNotification(-1);
+        String url = intent.getStringExtra(KEY_URL_SIGN);
+        int icon = intent.getIntExtra(KEY_ICON, -1);
+        notifyId = intent.getIntExtra(KEY_NOTIFY_ID, 1000);
+        tible = intent.getStringExtra(KEY_TIBLE);
+        ProgressNotification.getInstance().initNotification(mContext, icon, tible, notifyId);
         OkHttp3NetWork.submitDownloadFile(this, url, HttpRequestUtils.FILE_SAVE_CATALOGUE, new HttpRequestUtils.OnDownloadListener() {
             @Override
             public void onDownloadSuccess(File file) {
-                Log.i("zs", "请求成功");
-                cancelNotification();
+                Log.i(TAG, "请求成功");
+                ProgressNotification.getInstance().cancelNotification();
+                DownLoadService.this.stopSelf();
                 SystemUtils.getInstance().installApk(mContext, file);  // 安装软件
             }
 
             @Override
             public void onDownloading(int progress) {
-                updateNotification(progress);
+                ProgressNotification.getInstance().updateNotification(progress);
             }
 
             @Override
             public void onDownloadFailed() {
-                Log.i("zs", "请求失败");
-                cancelNotification();
+                Log.i(TAG, "请求失败");
+                ProgressNotification.getInstance().cancelNotification();
             }
         });
 
@@ -77,40 +88,4 @@ public class DownLoadService extends Service {
     }
 
 
-    /**
-     * 初始化Notification通知
-     */
-    public void initNotification(int icon) {
-        builder = new NotificationCompat.Builder(mContext)
-                .setContentText("0%")
-                .setContentTitle(tible)
-                .setProgress(100, 0, false);
-        if (icon != -1) {
-            builder.setSmallIcon(icon);
-        }
-
-        notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(notify_id, builder.build());
-    }
-
-    /**
-     * 更新通知
-     */
-    public void updateNotification(long progress) {
-        int currProgress = (int) progress;
-        if (preProgress < currProgress) {
-            builder.setContentText(progress + "%");
-            builder.setProgress(100, (int) progress, false);
-            notificationManager.notify(notify_id, builder.build());
-        }
-        preProgress = (int) progress;
-    }
-
-    /**
-     * 取消通知
-     */
-    public void cancelNotification() {
-        notificationManager.cancel(notify_id);
-        this.stopSelf();
-    }
 }
