ca4b7d66e79c384c67f20a11a7a866d3a1f74907
13674928326 <695584109@qq.com>
2018-12-15 14:58:12 +0800

    哦动态获取listview高

--- utils/src/main/java/com/library/utils/utils/ViewUtils.java
+++ utils/src/main/java/com/library/utils/utils/ViewUtils.java
@@ -162,6 +162,30 @@ public class ViewUtils {
         vectorDrawableCompat4.setTint(activity.getResources().getColor(colour)); //你需要改变的颜色
         return vectorDrawableCompat4;
     }
+    /**
+     * 获取ListView对应的Adapter
+     */
+    public void setListViewHeightBasedOnChildren(ListView listView) {
+        // 获取ListView对应的Adapter
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        int totalHeight = 0;
+        for (int i = 0, len = listAdapter.getCount(); i < len; i++) {
+            // listAdapter.getCount()返回数据项的数目
+            View listItem = listAdapter.getView(i, null, listView);
+            // 计算子项View 的宽高
+            listItem.measure(0, 0);
+            // 统计所有子项的总高度
+            totalHeight += listItem.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+//         listView.getDividerHeight();//获取子项间分隔符占用的高度
+//         params.height;//最后得到整个ListView完整显示需要的高度
+        listView.setLayoutParams(params);
+    }
 
     /**
      * 动态控制gridview的高度
