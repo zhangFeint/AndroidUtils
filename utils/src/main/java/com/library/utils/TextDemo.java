7522752ad7aa9b493c1941cafe7dea66de1a4c17
13674928326 <695584109@qq.com>
2019-01-15 09:42:35 +0800

    修改StringUtils

--- utils/src/main/java/com/library/utils/TextDemo.java
+++ utils/src/main/java/com/library/utils/TextDemo.java
@@ -13,10 +13,12 @@ import com.library.utils.utils.TimeUtils;
  */
 public class TextDemo {
     public static void main(String[] args) {
-        TimeUtils timeUtils = TimeUtils.getInstance();
-        String longtime = timeUtils.getTimestamp("2018-12-9 06:10:12", TimeUtils.TIME_FORMAT_2);
-        System.out.println(longtime);
-        System.out.println(timeUtils.getTimeStateNew(longtime, TimeUtils.TIME_FORMAT_10));
+//        TimeUtils timeUtils = TimeUtils.getInstance();
+//        String longtime = timeUtils.getTimestamp("2018-12-9 06:10:12", TimeUtils.TIME_FORMAT_2);
+//        System.out.println(longtime);
+//        System.out.println(timeUtils.getTimeStateNew(longtime, TimeUtils.TIME_FORMAT_10));
+       TimeUtils.TimeBean timeBean =  TimeUtils.getInstance().getStringData();
+        System.out.println(timeBean.getDay());
     }
 
 }
