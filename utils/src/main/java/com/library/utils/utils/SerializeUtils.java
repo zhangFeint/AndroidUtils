b53a93c9a139ca39547e7059a3ceded7e618722c
13674928326 <695584109@qq.com>
2019-01-23 13:21:57 +0800

    优化请求

--- utils/src/main/java/com/library/utils/utils/SerializeUtils.java
+++ utils/src/main/java/com/library/utils/utils/SerializeUtils.java
@@ -5,10 +5,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * 功能：
- *     序列化对象
+ * 序列化对象
+ *
  * @author：zhangerpeng
  * @create：2018\11\5 0005 15:06
  * @version：2018 1.0
@@ -21,41 +23,50 @@ public class SerializeUtils {
     /**
      * 序列化对象
      *
-     * @param object   对象必须实现 implements Serializable 接口
+     * @param object 对象必须实现 implements Serializable 接口
      * @return
      * @throws IOException
      */
-    public static String serialize(Object object) throws Exception {
-        startTime = System.currentTimeMillis();
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
-        objectOutputStream.writeObject(object);
-        String serStr = byteArrayOutputStream.toString("ISO-8859-1");
-        serStr = java.net.URLEncoder.encode(serStr, "UTF-8");
-        objectOutputStream.close();
-        byteArrayOutputStream.close();
-        endTime = System.currentTimeMillis();
+    public static String serialize(Object object) {
+        try {
+            startTime = System.currentTimeMillis();
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(object);
+            String serStr = byteArrayOutputStream.toString("ISO-8859-1");
+            serStr = java.net.URLEncoder.encode(serStr, "UTF-8");
+            objectOutputStream.close();
+            byteArrayOutputStream.close();
+            endTime = System.currentTimeMillis();
 //        System.out.println("序列化耗时为" + (endTime - startTime));
-        return serStr;
+            return serStr;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
     /**
      * 反序列化对象
      *
      * @param str
-     * @return  对象必须实现 implements Serializable 接口
+     * @return 对象必须实现 implements Serializable 接口
      * @throws Exception
-
      */
-    public static Object deSerialization(String str) throws Exception {
-        startTime = System.currentTimeMillis();
-        String redStr = java.net.URLDecoder.decode(str, "UTF-8");
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(redStr.getBytes("ISO-8859-1"));
-        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-        objectInputStream.close();
-        byteArrayInputStream.close();
-        endTime = System.currentTimeMillis();
+    public static Object deSerialization(String str) {
+        try {
+            startTime = System.currentTimeMillis();
+            String redStr = java.net.URLDecoder.decode(str, "UTF-8");
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(redStr.getBytes("ISO-8859-1"));
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            objectInputStream.close();
+            byteArrayInputStream.close();
+            endTime = System.currentTimeMillis();
 //        System.out.println("反序列化耗时为" + (endTime - startTime));
-        return objectInputStream.readObject();
+            return objectInputStream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Object();
+        }
     }
 }
